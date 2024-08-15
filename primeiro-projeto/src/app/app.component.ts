9eb16276646647dca1dd77f12539f072bf96538a
alicia]guiradelo <alicia.guiradelo@outlook.com>
2024-08-14 22:21:54 -0300

    adicionando o componente manualmente

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TerceiroComponentComponent } from "./terceiro-component/terceiro-component.component";
+import { MeuPrimeiroComponent } from "./meu-primeiro/meu-primeiro.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TerceiroComponentComponent],
+  imports: [RouterOutlet, TerceiroComponentComponent, MeuPrimeiroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
