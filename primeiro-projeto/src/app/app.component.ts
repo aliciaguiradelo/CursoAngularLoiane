b15be8102ab3a5376aec33dd0e8ba979bb33f454
alicia]guiradelo <alicia.guiradelo@outlook.com>
2024-08-14 22:16:15 -0300

    adicionando o terceiro componente na view

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TerceiroComponentComponent } from "./terceiro-component/terceiro-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TerceiroComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
