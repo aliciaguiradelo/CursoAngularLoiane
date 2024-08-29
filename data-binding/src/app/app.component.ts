5c40c9a7ccea13253864de94408890bb094c9aa1
alicia]guiradelo <alicia.guiradelo@outlook.com>
2024-08-29 20:42:14 -0300

    instalando biblioteca do bootstrap

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataComponentComponent } from "./data-component/data-component.component";
+import { ClassStyleBindingComponent } from "./class-style-binding/class-style-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataComponentComponent],
+  imports: [RouterOutlet, DataComponentComponent, ClassStyleBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
