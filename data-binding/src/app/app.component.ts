647ad68d4074240dcff7ed8c1d9f3a20425284c2
alicia]guiradelo <alicia.guiradelo@outlook.com>
2024-09-05 20:01:54 -0300

    finaliza aula de event bondong

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataComponentComponent } from "./data-component/data-component.component";
 import { ClassStyleBindingComponent } from "./class-style-binding/class-style-binding.component";
+import { EventBindingComponent } from "./event-binding/event-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataComponentComponent, ClassStyleBindingComponent],
+  imports: [RouterOutlet, DataComponentComponent, ClassStyleBindingComponent, EventBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- data-binding/src/app/event-binding/event-binding.component.ts
+++ data-binding/src/app/event-binding/event-binding.component.ts
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class EventBindingComponent {
 
+  valorAtual: string='';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
+
+  botaoClicado(){
+    alert('clicou no botão!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    //console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
 }
