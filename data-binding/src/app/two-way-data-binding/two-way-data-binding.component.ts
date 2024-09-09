23d20f4bf38226fcf351f690b4e8e49820683fec
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-09-09 17:11:50 -0300

    cria component para nova aula

---
+++ data-binding/src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-data-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './two-way-data-binding.component.html',
+  styleUrl: './two-way-data-binding.component.css'
+})
+export class TwoWayDataBindingComponent {
+
+}
