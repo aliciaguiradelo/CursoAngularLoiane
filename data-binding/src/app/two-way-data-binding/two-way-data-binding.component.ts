dad52a8517dde510c312a56f7956f466708280eb
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-09-09 20:30:28 -0300

    corrige imports do component standalone

--- data-binding/src/app/two-way-data-binding/two-way-data-binding.component.ts
+++ data-binding/src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-two-way-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './two-way-data-binding.component.html',
   styleUrl: './two-way-data-binding.component.css'
 })
 export class TwoWayDataBindingComponent {
-  nome: string = 'ABC'
+  nome: string = 'ABC';
+  pessoa: any = {
+    nome : 'def',
+    idade : 23
+
+  }
 
 }
