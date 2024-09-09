d8e3bce290564c6ed1ca1bf19a6622c2b99127da
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-09-09 20:44:24 -0300

    importa novo component no app.component

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { DataComponentComponent } from "./data-component/data-component.componen
 import { ClassStyleBindingComponent } from "./class-style-binding/class-style-binding.component";
 import { EventBindingComponent } from "./event-binding/event-binding.component";
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { InputOutptCompoComponent } from './input-outpt-compo/input-outpt-compo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataComponentComponent, ClassStyleBindingComponent, EventBindingComponent, TwoWayDataBindingComponent],
+  imports: [RouterOutlet, DataComponentComponent, ClassStyleBindingComponent, EventBindingComponent, TwoWayDataBindingComponent, InputOutptCompoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
